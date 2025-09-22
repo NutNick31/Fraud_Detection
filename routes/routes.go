69d0511478e6a57daacccc17e1654a30a64c608f
alt-afk <theebatmen@gmail.com>
2025-09-22 11:01:02 +0530

    initial commit

--- routes/routes.go
+++ routes/routes.go
@@ -9,12 +9,9 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	gdsGroup := r.Group("/gds")
-	{
-		gdsGroup.GET("/louvian/:id/:hop_length", getLouvianCluster)
-		gdsGroup.GET("/labelprop/:id/:hop_length", getLabelpropCluster)
-	}
+	
 	r.DELETE("/reset", resetDatabase)
+	r.GET("/components", getComponents)
 }
 
 func resetDatabase(c *gin.Context) {
@@ -35,101 +32,20 @@ func resetDatabase(c *gin.Context) {
 	c.JSON(200, gin.H{"status": "Database reset successful"})
 }
 
+func getComponents(c *gin.Context) {
 
-func getLouvianCluster(c *gin.Context) {
 	session := db.Driver.NewSession(c, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	defer session.Close(c)
-	
-	// label := c.Param("label")
-	id := c.Param("id")
-	hop := c.Param("hop_length")
 
-	params := map[string]interface{}{
-		"id":         id,
-		"hop_length": hop,
-	}
-
-	result, err := session.ExecuteWrite(c, func(tx neo4j.ManagedTransaction) (interface{}, error) {
-		records, err := tx.Run(c, db.QueryLouvian, params)
-		if err != nil {
-			return nil, err
-		}
-
-		// Map: communityId => list of {label, id}
-		clusters := make(map[int][]map[string]interface{})
-
-		for records.Next(c) {
-			record := records.Record()
-			communityID := int(record.Values[0].(int64))
-			label := record.Values[1].(string)
-			id := record.Values[2]
-
-			nodeInfo := map[string]interface{}{
-				"label": label,
-				"node_id":    id,
-			}
-
-			clusters[communityID] = append(clusters[communityID], nodeInfo)
-
-			fmt.Printf("Community %d: %+v\n", communityID, nodeInfo)
-		}
-		return clusters, nil
+	results, err := session.ExecuteWrite(c, func(tx neo4j.ManagedTransaction) (interface{}, error){
+		records, err := tx.Run(c, QueryComponents, nil)
+		return records, err
 	})
-
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+		c.JSON(500, gin.H{"error": err.Error()})
 	}
-
-	c.JSON(http.StatusOK, gin.H{"clusters": result})
+	c.JSON(200, gin.H{"Status": "Components Fetched Successfully"})
+	return results
 }
 
 
-func getLabelpropCluster(c *gin.Context) {
-		session := db.Driver.NewSession(c, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
-	defer session.Close(c)
-	
-	// label := c.Param("label")
-	id := c.Param("id")
-	hop := c.Param("hop_length")
-
-	params := map[string]interface{}{
-		"id":         id,
-		"hop_length": hop,
-	}
-
-	result, err := session.ExecuteWrite(c, func(tx neo4j.ManagedTransaction) (interface{}, error) {
-		records, err := tx.Run(c, db.QueryLabelprop, params)
-		if err != nil {
-			return nil, err
-		}
-
-		// Map: communityId => list of {label, id}
-		clusters := make(map[int][]map[string]interface{})
-
-		for records.Next(c) {
-			record := records.Record()
-			communityID := int(record.Values[0].(int64))
-			label := record.Values[1].(string)
-			id := record.Values[2]
-
-			nodeInfo := map[string]interface{}{
-				"label": label,
-				"node_id":    id,
-			}
-
-			clusters[communityID] = append(clusters[communityID], nodeInfo)
-
-			fmt.Printf("Community %d: %+v\n", communityID, nodeInfo)
-		}
-
-		return clusters, nil
-	})
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"clusters": result})
-}
