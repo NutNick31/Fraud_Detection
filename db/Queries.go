37311686e9a1ce1622fe2cb867d75eb99bf75f3e
Rajesh Kumar Singh <81872302+NutNick31@users.noreply.github.com>
2025-09-22 12:15:39 +0530

    Update Queries.go

--- db/Queries.go
+++ db/Queries.go
@@ -1,9 +1,11 @@
 package db
 const(
   QueryComponents = `
-    MATCH (n)
-    WHERE degree(n) > 1
-    CALL apoc.path.subgraphAll(n, {relationshipFilter: ">", minLevel:0}) YIELD nodes, relationships
-    RETURN n AS startNode, nodes AS componentNodes, relationships AS componentRels
+    MATCH (n)-[r]-()
+    WITH n, count(r) AS degree
+    WHERE degree > 1
+    CALL apoc.path.subgraphAll(n, {}) YIELD nodes, relationships
+    RETURN n AS startNode, nodes AS componentNodes, relationships AS componentRels;
+
   `
 )
